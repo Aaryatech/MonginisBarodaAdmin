d3fb200f0c0f7acc8a78e69c0b775661c6a77da2
ats-12 <ats-12@ats12>
2021-05-10 11:30:39 +0530

    //Shubham

---
+++ src/main/java/com/ats/adminpanel/model/PaymentMode.java
@@ -0,0 +1,43 @@
+package com.ats.adminpanel.model;
+
+
+public class PaymentMode {
+
+	private int modeId;
+	private String modeName;
+	private int delStatus;
+
+	public int getModeId() {
+		return modeId;
+	}
+
+	public void setModeId(int modeId) {
+		this.modeId = modeId;
+	}
+
+	public String getModeName() {
+		return modeName;
+	}
+
+	public void setModeName(String modeName) {
+		this.modeName = modeName;
+	}
+
+	public int getDelStatus() {
+		return delStatus;
+	}
+
+	public void setDelStatus(int delStatus) {
+		this.delStatus = delStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "PaymentMode [modeId=" + modeId + ", modeName=" + modeName + ", delStatus=" + delStatus + "]";
+	}
+	
+	
+	
+	
+
+}
